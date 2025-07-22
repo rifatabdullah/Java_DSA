9f5108ec600d3672d12185f4ad050c23004655c6
levia <rifatabdullah754@gmail.com>
2025-07-22 22:27:59 +0600

    22

--- Lab 4 - Secondary Data Structures [JAVA]/Assignment Tasks/AssignmentTask3/Node.java
+++
@@ -1,9 +0,0 @@
-//DO NOT CHANGE THIS CLASS
-class Node {
-    Object elem;
-    Node next;
-    public Node(Object elem) {
-        this.elem = elem;
-        this.next = null;
-    }
-}
--- Lab 4 - Secondary Data Structures [JAVA]/Lab Tasks/LabTask3/Node.java
+++
@@ -1,9 +0,0 @@
-//DO NOT CHANGE THIS CLASS
-class Node {
-    int elem;
-    Node next;
-    public Node(int elem) {
-        this.elem = elem;
-        this.next = null;
-    }
-}
